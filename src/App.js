d8522a508bae80d0eebfa3551dc1356c82e47335
Prajwal C <prajwalc.sagar22@gmail.com>
2023-07-27 19:10:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import { Footer ,Blog ,Possibility ,Features ,WhatGPT3 ,Header } from './containers';
+import {  Brand ,Cta ,Navbar } from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+          <Brand />
+          <WhatGPT3 />
+          <Features />
+          <Possibility />
+          <Cta />
+          <Blog />
+          <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default App
